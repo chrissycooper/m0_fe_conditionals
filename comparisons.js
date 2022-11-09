a0f7d8f86c33ff0bef590d87a4150d033f95966d
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-11-09 13:28:28 -0600

    Complete Part 2: Articulating

--- comparisons.js
+++ comparisons.js
@@ -58,22 +58,26 @@ console.log("is numberStudents less than or equal to 21", numberStudents <= 21);
 // Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 // Make sure YOU can explain it that way!
 
-console.log(4 < 9);
+console.log("4 < 9", 4 < 9);
 // YOU DO: Explain.
+//In this code snippet, we are logging to the console (e.g. in this instance, this would show up in the terminal if I ran this code via `node` command) the value of the inequality 4 < 9. This is comparing the value 4 as a number to the value 9 as a number. 4 *is* less than 9, so true should show up in the terminal. 
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+//This is similar to the last example, but we have a variable in place of the 9 we had before. In this instance, the code is evaluating whether 4 is less than the value of the variable books, which is 3. 4 is greater than 3, so false should show up in the terminal.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+// This one adds on to the last example. here the code is comparing two variables. It's asking, "Is the value of friends greater than the value of siblings?" 6 is greater than 2, so true should be logged to the console.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
+// Here, again we have the values of two variables being compared, but in a different way. This one is asking, "Is the value of attendees not strictly equal to the value of meals?" 9 is not equal to 8, so true should be logged to the console. 
 
 
 // #-------------------
